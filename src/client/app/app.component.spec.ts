e11d6bff9d9aa8966cd99f6cf7c76f9f4ff74bd6
Roman Chaley <romanchaley@gmail.com>
2016-09-05 00:14:01 +0300

    Fixed angular2 tests, started working on server tests

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -6,6 +6,7 @@ describe('App', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({ declarations: [AppComponent]});
     });
+
     it ('should work', () => {
         let fixture = TestBed.createComponent(AppComponent);
         expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
---
+++ tests/common.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('some test', function () {
+    it('should work', function () {
+        expect(2 + 2).toBe(4);
+    });
+});
