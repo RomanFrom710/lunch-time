79905a4a0fb137300f1d6539af797efbb577014a
Roman Chaley <romanchaley@gmail.com>
2016-09-06 00:01:41 +0300

    Added mock for app header

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'lunch-time-app',
+    selector: 'lt-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
