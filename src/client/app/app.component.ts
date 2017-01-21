bcdb4ac4f0c18fcca30fb7f9e82a49e116d88e30
Roman Chaley <romanchaley@gmail.com>
2017-01-21 18:22:52 +0300

    Fixed toastr

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+import { ToastsManager } from 'ng2-toastr';
 
 
 @Component({
     selector: 'lt-app',
     templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    constructor(toastsManager: ToastsManager, viewContainer: ViewContainerRef) {
+        toastsManager.setRootViewContainerRef(viewContainer);
+    }
+}
