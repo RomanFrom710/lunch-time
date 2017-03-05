abb8347addd392d1e2f89acb235e9129d5eeb48f
Roman Chaley <romanchaley@gmail.com>
2017-03-05 13:15:41 +0300

    Fixed DI error

--- src/client/app/shared/errors/error-handler.service.ts
+++ src/client/app/shared/errors/error-handler.service.ts
@@ -3,6 +3,10 @@ import { Injectable, ErrorHandler } from '@angular/core';
 
 @Injectable()
 export class CustomErrorHandler extends ErrorHandler {
+    constructor() {
+        super(false);
+    }
+
     handleError(error: any): void {
         console.log(error); // todo: Add logging
     }
