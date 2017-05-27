d611189b22dc8df674473ca23fe4746ed778f7a2
Roman Chaley <romanchaley@gmail.com>
2017-05-27 14:07:56 +0300

    Added tests coverage; added some markup

--- src/client/app/info/main/main-page.component.ts
+++ src/client/app/info/main/main-page.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'lt-main-page',
-    templateUrl: 'main-page.component.html'
+    templateUrl: 'main-page.component.html',
+    styleUrls: ['main-page.component.less']
 })
 export class MainPageComponent { }
